c751c5d15aaf3a94d8651b66780f02e3ffa44972
JaryJay <47123100+JaryJay@users.noreply.github.com>
2021-03-02 16:42:04 -0500

    Logic now makes a state every frame, visuals displays entities smoothly

--- src/main/java/bundle/game/DuelsGameLogic.java
+++ src/main/java/bundle/game/DuelsGameLogic.java
@@ -1,12 +1,21 @@
 package bundle.game;
 
 import bundle.logic.AbstractGameLogic;
+import input.InputFrame;
+import misc.LimitedQueue;
+import state.GameState;
 
 public class DuelsGameLogic extends AbstractGameLogic {
 
+	private boolean addNewFrame = true;
+
 	@Override
 	public void update() {
-
+		if (addNewFrame) {
+			DuelsGameData data = (DuelsGameData) getBundle().getData();
+			LimitedQueue<GameState> states = data.getStates();
+			states.add(states.getLast().getNextState(new InputFrame()));
+		}
 	}
 
 }
--- src/main/java/bundle/game/DuelsGameVisuals.java
+++ src/main/java/bundle/game/DuelsGameVisuals.java
@@ -3,16 +3,20 @@ package bundle.game;
 import bundle.visuals.AbstractGameVisuals;
 import entity.Player;
 import entity.PlayerDisplayer;
+import entity.Projectile;
+import entity.ProjectileDisplayer;
 import state.GameState;
 
 public class DuelsGameVisuals extends AbstractGameVisuals {
 
 	private PlayerDisplayer playerDisplayer;
+	private ProjectileDisplayer projectileDisplayer;
 
 	@Override
 	public void init() {
 		super.init();
 		playerDisplayer = new PlayerDisplayer(getBundle().getWrapper().getRenderer());
+		projectileDisplayer = new ProjectileDisplayer(getBundle().getWrapper().getRenderer());
 	}
 
 	@Override
@@ -22,9 +26,16 @@ public class DuelsGameVisuals extends AbstractGameVisuals {
 	@Override
 	public void displayBeforeGUIS() {
 		DuelsGameData data = (DuelsGameData) getBundle().getData();
+		System.out.println(data.getStates().size());
 		GameState currentState = data.getStates().getLast();
+		float alpha = getBundle().getWrapper().getLogicTimer().getAlpha();
 		for (Player d : currentState.getPlayers()) {
-			playerDisplayer.display(d);
+//			playerDisplayer.display(d);
+			playerDisplayer.display(d, alpha);
+		}
+		for (Projectile p : currentState.getProjectiles()) {
+//			projectileDisplayer.display(p);
+			projectileDisplayer.display(p, alpha);
 		}
 	}
 
